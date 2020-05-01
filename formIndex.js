8fe0bdab961b044cba13a5867e9fbcf4901060b4
philohelp <thpoirier@gmail.com>
2020-05-01 15:15:59 +0200

    2 files modified

--- formIndex.js
+++ formIndex.js
@@ -1,49 +1,94 @@
 import React from "react";
 
 import {
-  diapo,
   btmImg,
   rightImg,
   leftImg,
-  singleImg,
   twoCols,
-  topImgTwoCols,
-  twoFold
+  oneImgTwoCols,
+  twoFold,
+  singleImg,
+  imgSlider,
+  singleVid,
+  vidSlider
 } from "./images";
 
 import * as colors from "./colors";
 
-const imArray = [
-  leftImg,
-  rightImg,
-  btmImg,
-  singleImg,
-  diapo,
-  twoCols,
-  topImgTwoCols,
-  twoFold
+const imArray = [];
+
+const singleArray = [
+  { id: "leftImg", title: "Image à gauche", img: leftImg },
+  { id: "rightImg", title: "Image à droite", img: rightImg },
+  { id: "btmImg", title: "Image en bas", img: btmImg }
 ];
+
+const doubleArray = [
+  { id: "twoCols", title: "Aucune image", img: twoCols },
+  { id: "oneImgTwoCols", title: "Une seule image", img: oneImgTwoCols },
+  { id: "twoFold", title: "Deux images", img: twoFold }
+];
+
+const imageArray = [
+  { id: "singleImg", title: "Grande image traversante", img: singleImg },
+  { id: "imgSlider", title: "Slider images", img: imgSlider }
+];
+
+const videoArray = [
+  { id: "singleVid", title: "Grande vidéo traversante", img: singleVid },
+  { id: "vidSlider", title: "Slider vidéos", img: vidSlider }
+];
+
+const groups = {
+  single: { id: "single", items: imageArray, title: "Une colonne" },
+  double: { id: "double", items: videoArray, title: "Double colonne" },
+  image: { id: "image", items: imageArray, title: "Images" },
+  video: { id: "video", items: videoArray, title: "Vidéos" }
+};
 
 export default props => {
   return (
     <div>
-    <div style={{width:650, display:"flex", flexWrap:"wrap"}}>
-      {imArray.map(im => (
-        <div
-          key={im}
-          style={{
-            margin:10,
-            width: 80,
-            height: 80,
-            paddingTop: 3,
-            paddingLeft: 3,
-            border: `1px solid ${colors.grisf}`
-          }}
-        >
-          <img src={im} style={{ maxHeight: 75 }} />
+      <div style={{ width: 600, display: "flex" }}>
+        <Group />
+        <div style={{ display: "flex" }}>
+          {imageArray.map(item => (
+            <div>
+              <Item key={item.id} item={item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Group = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      {singleArray.map(item => (
+        <div>
+          <Item key={item.id} item={item} />
         </div>
       ))}
-      </div>
+    </div>
+  );
+};
+
+const Item = props => {
+  const { item } = props;
+  return (
+    <div
+      style={{
+        margin: 10,
+        width: 80,
+        height: 80,
+        paddingTop: 3,
+        paddingLeft: 3,
+        border: `1px solid ${colors.grisf}`
+      }}
+    >
+      <img src={item.img} style={{ maxHeight: 75 }} />
     </div>
   );
 };
--- images/index.js
+++ images/index.js
@@ -6,12 +6,16 @@ export const leftImg = `${imgUrl}/leftImg.png`
 
 export const rightImg = `${imgUrl}/rightImg.png`
 
-export const singleImg = `${imgUrl}/singleImg.png`
-
-export const diapo = `${imgUrl}/diapo.png`
-
 export const twoCols = `${imgUrl}/twoCols.png`
 
-export const topImgTwoCols = `${imgUrl}/topImgTwoCols.png`
+export const oneImgTwoCols = `${imgUrl}/topImgTwoCols.png`
 
 export const twoFold = `${imgUrl}/twoFold.png`
+
+export const singleImg = `${imgUrl}/singleImg.png`
+
+export const imgSlider = `${imgUrl}/diapo.png`
+
+export const singleVid = `${imgUrl}/video.png`
+
+export const vidSlider = `${imgUrl}/videos.png`
